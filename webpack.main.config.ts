7a99cca853927e80eafd38c1b9136a1e591cbc4d
Lucas Neiva <lucasneiva07@gmail.com>
2025-05-20 15:46:32 -0300

    addd .env to webpack

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -1,5 +1,10 @@
 import type { Configuration } from 'webpack';
 import path from "path"
+import webpack from 'webpack';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 import { rules } from './webpack.rules';
 
@@ -13,6 +18,16 @@ export const mainConfig: Configuration = {
   module: {
     rules,
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.CLERK_API_SECRET_KEY': JSON.stringify(process.env.CLERK_API_SECRET_KEY),
+      'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+      'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      'process.env.LOGIN_URL': JSON.stringify(process.env.LOGIN_URL),
+    }),
+  ],
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
     alias: {
